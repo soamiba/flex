ebf000eeccca8a534e39e51293e9d9afff3c8706
soso <soamiba@hotmail.com>
2016-06-05 12:51:22 +0000

    flex grid

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -5,46 +5,47 @@ var wrap = require('gulp-wrap');
 var browserSync = require('browser-sync');
 
 
-gulp.task('browser-sync',['build','sass','cp'],function() {
-	browserSync({
-		server:{
-			baseDir: '..'
-		}
-	})
-})
+gulp.task('browser-sync', ['build', 'sass', 'cp'], function() {
+    browserSync({
+        server: {
+            baseDir: '..'
+        }
+    });
+});
 
-gulp.task('cp',function() {
-	return gulp.src('js/main.js',{ base: '.'})
-	pipe(gulp.dest('..'));
-})
+gulp.task('cp', function () {
+  return gulp.src(['js/main.js','assets/*'], { base: '.' })
+         .pipe(gulp.dest('..'));
+});
 
-gulp.task('build',function () {
-	gulp.src("pages/*.html")
-	    .pipe(wrap({src:"layout/default.html"}))
-	    .pipe(gulp.dest(".."));
-})
+gulp.task('build', function(){
+  gulp.src("pages/*.html")
+      .pipe(wrap({src:"layout/default.html"}))
+      .pipe(gulp.dest(".."));
+});
 
 function handleError(err) {
-	console.log(err.toString());
-	this.emit('end');
+  console.log(err.toString());
+  this.emit('end');
 }
+
 
 gulp.task('sass', function(){
   gulp.src('styles/main.scss')
-      .pipe(sass()).on('error',handleError)
+      .pipe(sass()).on('error', handleError)
       .pipe(prefix())
       .pipe(gulp.dest('../styles'))
-      .pipe(browserSync.reload({stream:true}));
+      .pipe(browserSync.reload({stream:true}));;
 });
 
-gulp.task('rebuild',['build'],function() {
-	browserSync.reload();
+gulp.task('rebuild', ['build'], function () {
+    browserSync.reload();
+});
+
+gulp.task('watch', function(){
+  gulp.watch(['**/*.html'], ['rebuild']);
+  gulp.watch(['styles/*.scss'], ['sass']);
+  gulp.watch(['js/main.js'], ['cp']);
 })
 
-gulp.task('watch',function(){
-	gulp.watch(['**/*.html'],['rebuild']);
-	gulp.watch(['styles/*.scss'],['sass']);
-	gulp.watch(['js/main.js'],['cp']);
-})
-
-gulp.task('default',['browser-sync','watch']);
+gulp.task('default', ['browser-sync', 'watch']);
